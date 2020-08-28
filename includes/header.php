ed57ccc76046464f60ba62af4befcb21898aeea3
BarenMaulana <barenmaulana@gmail.com>
2020-08-28 15:57:19 +0700

    menambahkan template

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<?php
+require 'init.php';
+
+$minings = getAll('mining_tb');
+?>
+
+<!doctype html>
+<html lang="en">
+
+<body>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link rel="stylesheet" href="style/style.css">
+        <title>PostrgeSQL CRUD</title>
+    </head>
--- index.php
+++ index.php
@@ -1,65 +1,60 @@
 <?php
-require 'init.php';
-
-$minings = getAll('mining_tb');
+require 'includes/header.php';
 ?>
 
-<!doctype html>
-<html lang="en">
+<div class="container my-4">
+    <h3>CRUD | PostgreSQL</h3>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" href="style/style.css">
-    <title>PostrgeSQL CRUD</title>
-</head>
 
-<body>
-    <div class="container my-4">
-        <h3>CRUD | PostgreSQL</h3>
-
-        <div class="row custom-alert">
-            <div class="col">
-            </div>
-        </div>
-
-        <div class="card shadow">
-            <div class="card-body">
-                <table class="table">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Mine result</th>
-                            <th scope="col">Mine color</th>
-                            <th scope="col">Mining date</th>
-                            <th scope="col">Mine owner</th>
-                            <th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($minings as $key => $mining) : ?>
-                            <tr>
-                                <th scope="row"><?= $key + 1 ?></th>
-                                <td><?= $mining['mine_result'] ?></td>
-                                <td><?= $mining['mine_color'] ?></td>
-                                <td><?= $mining['install_date'] ?></td>
-                                <td><?= $mining['mine_owner'] ?></td>
-                                <td>
-                                    <a href="#" class="btn btn-primary shadow"><img src="icon/pencil.svg" width="20"></a>
-                                    <a href="#" class="btn btn-danger shadow"><img src="icon/trash.svg" width="20"></a>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
+    <div class="row custom-alert d-none">
+        <div class="col">
         </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-</body>
+    <div class="row">
+        <div class="col">
+            <div class="card shadow">
+                <div class="card-body">
+                    <table class="table">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Mine result</th>
+                                <th scope="col">Mine color</th>
+                                <th scope="col">Mining date</th>
+                                <th scope="col">Mine owner</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($minings as $key => $mining) : ?>
+                                <tr>
+                                    <th scope="row"><?= $key + 1 ?></th>
+                                    <td><?= $mining['mine_result'] ?></td>
+                                    <td><?= $mining['mine_color'] ?></td>
+                                    <td><?= $mining['install_date'] ?></td>
+                                    <td><?= $mining['mine_owner'] ?></td>
+                                    <td>
+                                        <a href="#" class="btn btn-primary shadow"><img src="icon/pencil.svg" width="20"></a>
+                                        <a href="#" class="btn btn-danger shadow"><img src="icon/trash.svg" width="20"></a>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="col col-2 text-center side-navbar">
+            <div class="card shadow">
+                <div class="card-body">
+                    <a href="#" class="btn btn-primary btn-sm">Add new</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-</html>
+<?php
+require 'includes/footer.php';
+?>
